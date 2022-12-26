d95125f9f791303d931d942dbc2c6bfa5fc83c60
5x <longhorn482653@gmail.com>
2022-12-26 11:44:12 +0800

    style(app.js): prettier auto format

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /**
-* Slack App Setting
-* Enable Socket Mode
-* Enable Event Subscriptions
-* OAuth & Permissions: app_mentions:read, chat:write
-* @slack/bolt requires at least NodeJs version 12.13.0
-*/
-const { App } = require('@slack/bolt');
-const request = require('request');
-require('dotenv').config();
+ * Slack App Setting
+ * Enable Socket Mode
+ * Enable Event Subscriptions
+ * OAuth & Permissions: app_mentions:read, chat:write
+ * @slack/bolt requires at least NodeJs version 12.13.0
+ */
+const { App } = require("@slack/bolt");
+const request = require("request");
+require("dotenv").config();
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -16,51 +16,55 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
   // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
   // you still need to listen on some port!
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 });
 
-function callOpenAi(message){
+function callOpenAi(message) {
   return new Promise((resolve, reject) => {
-    request.post({
-      url: 'https://api.openai.com/v1/completions',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${OPENAI_API_KEY}`
+    request.post(
+      {
+        url: "https://api.openai.com/v1/completions",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPENAI_API_KEY}`,
+        },
+        json: {
+          model: "text-davinci-003",
+          prompt: message,
+          temperature: 0.7,
+          max_tokens: 1000,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        },
       },
-      json: {
-        model: 'text-davinci-003',
-        prompt: message,
-        temperature: 0.7,
-        max_tokens: 1000,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
+      (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const answer = body.choices[0].text;
+          resolve(answer);
+        }
       }
-    }, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const answer = body.choices[0].text;
-        resolve(answer);
-      }
-    });
+    );
   });
 }
 
-app.event('app_mention', async ({ event, context, client, say }) => {
+app.event("app_mention", async ({ event, context, client, say }) => {
   try {
     let answer = await callOpenAi(event.text);
-    await say({"blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `<@${event.user}> ${answer}`
-        }
-      }
-    ]});
-  }
-  catch (error) {
+    await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `<@${event.user}> ${answer}`,
+          },
+        },
+      ],
+    });
+  } catch (error) {
     console.error(error);
   }
 });
@@ -69,5 +73,5 @@ app.event('app_mention', async ({ event, context, client, say }) => {
   // Start your app
   await app.start();
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
