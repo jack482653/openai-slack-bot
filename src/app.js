8cfcd4cdbbf48d4eb36c80406ad2f1cc644ec433
5x <longhorn482653@gmail.com>
2023-03-13 19:00:37 +0800

    feat(src/app.js): handle singal

--- src/app.js
+++ src/app.js
@@ -132,6 +132,25 @@ app.command("/gen_image", async ({ command, ack, say }) => {
   }
 });
 
+async function shutdown() {
+  const timeout = setTimeout(() => {
+    logger.error("Stop server timed out, force exit");
+    process.exit(1);
+  }, 3000);
+  logger.log("Stopping server ...");
+  await app.stop();
+  clearTimeout(timeout);
+  process.exit(0);
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+process.on("SIGBREAK", shutdown);
+process.on("unhandledRejection", (error) => {
+  logger.error("Unhandled rejection: ", error);
+  process.exit(1);
+});
+
 (async () => {
   // Start your app
   await app.start();
