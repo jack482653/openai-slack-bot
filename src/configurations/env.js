15391b8c77e4b589b4a0edb8aa709b2aa2491d90
5x <longhorn482653@gmail.com>
2023-04-29 17:50:41 +0800

    feat(configurations/env.js): add openAI.chat.model

--- src/configurations/env.js
+++ src/configurations/env.js
@@ -22,6 +22,7 @@ module.exports = {
   openAI: {
     apiKey: process.env.OPENAI_API_KEY,
     chat: {
+      model: process.env.OPENAI_CHAT_MODEL,
       enableSummarize: process.env.OPENAI_CHAT_ENABLE_SUMMARIZE === "true",
       numOfMessages: getInt("OPENAI_CHAT_NUM_OF_MESSAGES", 2),
       ttl: getInt("OPENAI_CHAT_TTL", null),
