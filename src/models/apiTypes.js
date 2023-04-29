283fdd3e312dedbd2443d0160ef995e7bc6011bf
5x <longhorn482653@gmail.com>
2023-04-29 17:53:02 +0800

    feat(models/apiTypes.js): add OpenAI API tpyes

---
+++ src/models/apiTypes.js
@@ -0,0 +1,13 @@
+const Enum = require("@5x/enumjs");
+
+const apiTypes = new Enum();
+apiTypes.defineEnumProperty("CHAT", "chat");
+apiTypes.defineEnumProperty("COMPLETION", "completion");
+apiTypes.defineEnumProperty("EDITS", "edits");
+apiTypes.defineEnumProperty("TRANSCRIPTIONS", "transcriptions");
+apiTypes.defineEnumProperty("TRANSLATIONS", "translations");
+apiTypes.defineEnumProperty("FINE_TUNES", "fine-tunes");
+apiTypes.defineEnumProperty("EMBEDDINGS", "embeddings");
+apiTypes.defineEnumProperty("MODERATIONS", "moderations");
+
+module.exports = apiTypes;
