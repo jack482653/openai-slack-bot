5e2a92b348c9189fb3c92620ea2a96ae2fdc3037
5x <longhorn482653@gmail.com>
2023-04-29 17:53:24 +0800

    feat(errors/InvalidModelError.js): add InvalidModelError

---
+++ src/errors/InvalidModelError.js
@@ -0,0 +1,12 @@
+const models = require("../models/models");
+
+class InvalidModelError extends Error {
+  constructor(configName, currentModel) {
+    super(
+      `${configName} must be one of ${models.values()}. Current model: ${currentModel}.`
+    );
+    this.name = "InvalidModelError";
+  }
+}
+
+module.exports = InvalidModelError;
