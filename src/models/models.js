81d4790d310681ecb9201df1c6055af369fb5cf8
5x <longhorn482653@gmail.com>
2023-06-14 13:37:27 +0800

    feat(models/models.js): add new gpt models
    gpt-4-0613, gpt-4-32k-0613, gpt-3.5-turbo-0613, gpt-3.5-turbo-16k, gpt-3.5-turbo-16k-0613

--- src/models/models.js
+++ src/models/models.js
@@ -3,19 +3,40 @@ const apiTypes = require("./apiTypes");
 
 const models = new Enum();
 // /v1/chat/completions:
-// gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
+// gpt-4, gpt-4-0314, gpt-4-0613
+// gpt-4-32k, gpt-4-32k-0314, gpt-4-32k-0613
+// gpt-3.5-turbo, gpt-3.5-turbo-0301, gpt-3.5-turbo-0613
+// gpt-3.5-turbo-16k, gpt-3.5-turbo-16k-0613
 models.defineEnumProperty("GPT_4", "gpt-4", { types: [apiTypes.CHAT] });
+// Discontinued date: 2023/09/13
 models.defineEnumProperty("GPT_4_0314", "gpt-4-0314", {
   types: [apiTypes.CHAT],
 });
+models.defineEnumProperty("GPT_4_0613", "gpt-4-0613", {
+  types: [apiTypes.CHAT],
+});
 models.defineEnumProperty("GPT_4_32K", "gpt-4-32k", { types: [apiTypes.CHAT] });
+// Discontinued date: 2023/09/13
 models.defineEnumProperty("GPT_4_32K_0314", "gpt-4-32k-0314", {
+  types: [apiTypes.CHAT],
+});
+models.defineEnumProperty("GPT_4_32K_0613", "gpt-4-32k-0613", {
   types: [apiTypes.CHAT],
 });
 models.defineEnumProperty("GPT_3_5_TURBO", "gpt-3.5-turbo", {
   types: [apiTypes.CHAT],
 });
+// Discontinued date: 2023/09/13
 models.defineEnumProperty("GPT_3_5_TURBO_0301", "gpt-3.5-turbo-0301", {
+  types: [apiTypes.CHAT],
+});
+models.defineEnumProperty("GPT_3_5_TURBO_0613", "gpt-3.5-turbo-0613", {
+  types: [apiTypes.CHAT],
+});
+models.defineEnumProperty("GPT_3_5_TURBO_16K", "gpt-3.5-turbo-16k", {
+  types: [apiTypes.CHAT],
+});
+models.defineEnumProperty("GPT_3_5_TURBO_16K_0613", "gpt-3.5-turbo-16k-0613", {
   types: [apiTypes.CHAT],
 });
 // text-davinci-003, text-davinci-002, text-curie-001, text-babbage-001, text-ada-001
