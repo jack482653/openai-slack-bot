da5a81b0b656575d3bebb8786e2cbd6fba732d9a
5x <longhorn482653@gmail.com>
2023-06-19 11:37:37 +0800

    feat(configurations/openai.js): add azure provider support

--- src/configurations/openai.js
+++ src/configurations/openai.js
@@ -2,11 +2,36 @@ const { Configuration, OpenAIApi } = require("openai");
 const cache = require("./cache");
 const env = require("./env");
 const OpenAICommand = require("#models/OpenAICommand");
+const providers = require("#models/providers");
 
-const configuration = new Configuration({
-  apiKey: env.openAI.apiKey,
-  basePath: "https://oai.hconeai.com/v1",
-});
+const getConfig = (provider, auth) => {
+  switch (provider) {
+    case providers.AZURE:
+      return {
+        apiKey: auth.apiKey,
+        basePath: auth.basePath,
+        baseOptions: {
+          headers: { "api-key": auth.apiKey },
+          params: {
+            "api-version": auth.apiVersion,
+          },
+        },
+      };
+    case providers.OPENAI:
+      return {
+        apiKey: auth.apiKey,
+        basePath: auth.basePath,
+      };
+    default:
+      return {
+        apiKey: auth.apiKey,
+      };
+  }
+};
+
+const configuration = new Configuration(
+  getConfig(env.openAI.provider, env.openAI.auth)
+);
 const openAIApi = new OpenAIApi(configuration);
 const openAICommand = new OpenAICommand(openAIApi, cache, env.openAI);
 
